21767d2261ac7305c6e88f58b0dce6af619b94b8
BRAYAN ORTEGA ARMENDARIZ <a348572@uach.mx>
2024-02-12 15:52:21 -0600

    realizando cambios en el servidor, agregando setInterval

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ const fs = require('fs');
 
 // Define the request handler function
 const requestHandler = (request, response) => {
-  // Read the message from a file asynchronously
-  fs.readFile('message.txt', 'utf8', (err, data) => {
-    if (err) {
-      response.writeHead(500, {'Content-Type': 'text/plain'});
-      response.end('Internal Server Error');
-    } else {
-      response.writeHead(200, {'Content-Type': 'text/plain'});
-      response.end(data);
-    }
-  });
+
+  // Simulate an asynchronous operation with setTimeout
+  setTimeout(() => {
+    // Read a message from a file asynchronously to demonstrate server operation
+    fs.readFile('message.txt', 'utf8', (err, data) => {
+      if (err) {
+        response.writeHead(500, {'Content-Type': 'text/plain'});
+        response.end('Internal Server Error');
+      } else {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end(data);
+      }
+    });
+  }, 2000); // Delay for 2 seconds
 };
 
 // Create the server
